e0588a3fd19b2669251fc7e8e2ecba8ee1e3f47c
abhishekkumar-1212 <129652028+abhishekkumar-1212@users.noreply.github.com>
2024-07-09 14:54:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let capture;
+let noseX , noseY;
+let  reyeX, reyeY;
+let leyeX, leyeY;
 let singlePose;
-let skeleton;
-
+let skeleton;       // Skeleton is the 2d array . 
+       
 function setup() {
   createCanvas(800, 500);
   capture = createCapture(VIDEO);
@@ -13,10 +16,16 @@ function setup() {
 }
 
 function receivedPoses(poses) {
+  console.log(poses);
+
   if (poses.length > 0) {
     singlePose = poses[0].pose; // keypoints come inside the single poses
-    skeleton = poses[0].skeleton;
+  skeleton=poses[0].skeleton;
+
+
   }
+
+  console.log(noseX + " " + noseY);
 }
 
 function modelLoaded() {
@@ -27,17 +36,20 @@ function draw() {
   background(0); // Clear the canvas with a black background (or any color you prefer)
   image(capture, 0, 0, 800, 500); // Draw the video capture on the canvas
 
-  if (singlePose) {
-    fill(255, 102, 102); // Light blue color code
-    // Draw keypoints
-    for (let i = 0; i < singlePose.keypoints.length; i++) {
-      ellipse(singlePose.keypoints[i].position.x, singlePose.keypoints[i].position.y, 15, 16);
-    }
-    stroke(255); // for color
-    strokeWeight(5);
-    // Draw skeleton
-    for (let j = 0; j < skeleton.length; j++) {
-      line(skeleton[j][0].position.x, skeleton[j][0].position.y, skeleton[j][1].position.x, skeleton[j][1].position.y);
-    }
+  fill(255, 102, 102); // Light blue color code
+  
+  // to detect all the body part we traverse keypoints by using the for loop 17 times loops is going on 
+  if (singlePose){
+  for( let i=0;i<singlePose.keypoints.length;i++){
+ellipse(singlePose.keypoints[i].position.x,singlePose.keypoints[i].position.y, 15,16);
   }
+  stroke(255,255); // for color 
+  strokeWeight(5);
+  for(let j=0;j<skeleton.length;j++){
+    line(skeleton[j][0].position.x, skeleton[j][0].position.y,skeleton[j][1].position.x,skeleton[j][1].position.y);
+  }
+
+
+  }
+  
 }
